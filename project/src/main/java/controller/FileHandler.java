2b352ed675d2fc744cd28c40d6959386af007bed
braha <braha@192.168.10.150>
2021-04-16 17:53:09 +0200

    testting

--- project/src/main/java/controller/FileHandler.java
+++ project/src/main/java/controller/FileHandler.java
@@ -13,7 +13,6 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
-
 public class FileHandler implements FileHandlerInterface{
 	
 	public static BoardController game;
--- project/src/test/java/test/FIleHandlerTest.java
+++ project/src/test/java/test/FIleHandlerTest.java
@@ -2,14 +2,18 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.FileNotFoundException;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import controller.FileHandler;
+import model.BoardModel;
 
 public class FIleHandlerTest {
 	
 	private FileHandler filehandler;
+	private BoardModel boardmodel;
 	
 	@BeforeEach
 	public void setup() {
@@ -18,7 +22,15 @@ public class FIleHandlerTest {
 	
 	@Test
 	public void test() {
-		
+		FileHandler savedNewGame; // Required to ignore Eclipse warning
+//		try {
+//			savedNewGame = filehandler.getScoresFromFile("test-save");
+//		} catch (FileNotFoundException e) {
+//			fail("Could not load saved file");
+//			return;
+//		}
+//		assertEquals(filehandler.toString(), savedNewGame.toString());
+//		assertFalse(boardmodel.getIsGameOver());
 	}
 }
 
